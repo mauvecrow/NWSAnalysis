70bd1f7842f17bf8e3c026cf3b6d3e997816d6a0
Bradley Quangson <bradley.quangson@gmail.com>
2023-10-07 12:00:58 -0600

    Create an interface to represent the api calls planned to get forecasts

---
+++ NWSAnalysis/Server/Services/INwsService.cs
@@ -0,0 +1,12 @@
+﻿using NWSAnalysis.Shared.Models;
+namespace NWSAnalysis.Server.Services;
+
+public interface INwsService
+{
+    // Call /stations
+    Task<StationsDto> GetStations(string state);
+    // Call /stations/{id}
+    Task<StationDto> GetStation(string stationId);
+    // Call /stations/{id}/observations
+    Task<Observations> GetStationObservations(string stationId);
+}
