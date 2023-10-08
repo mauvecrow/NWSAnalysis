defe9953234772aaf83302928150a19185bf1af4
Bradley Quangson <bradley.quangson@gmail.com>
2023-10-07 19:04:56 -0600

    implement client side api

---
+++ NWSAnalysis/Server/Services/ApiService.cs
@@ -0,0 +1,50 @@
+﻿using NWSAnalysis.Shared.Models;
+
+namespace NWSAnalysis.Server.Services;
+
+public class ApiService : IApiService
+{
+    private readonly INwsService _service;
+
+    public ApiService(INwsService service)
+    {
+        _service = service;
+    }
+
+    public async Task<ForecastDetails?> GetStationNames(string stateCode)
+    {
+
+        var dto = await _service.GetStations(stateCode);
+        if(dto != null)
+        {
+            var details = new ForecastDetails();
+            var dictionary = new Dictionary<string, string>();
+            foreach (var f in dto.features)
+            {
+                dictionary.Add(f.properties.stationIdentifier, f.properties.name);
+            }
+            details.StationNames = dictionary;
+            details.StateCode = stateCode;
+            return details;
+        }
+        return default;
+
+    }
+
+    public async Task<Dictionary<DateTime, float?>> GetStationForecasts(string stationId)
+    {
+        var dto = await _service.GetStationObservations(stationId);
+        if(dto != null)
+        {
+            var readings = new Dictionary<DateTime, float?>();
+            foreach (var f in dto.features)
+            {
+                readings.Add(f.properties.timestamp, f.properties.temperature.value);
+            }
+            return readings;
+        }
+        return default;
+    }
+
+
+}
