cc86afd7dca1adb1b74ffbad51f2e8d6e0b276a0
Bradley Quangson <bradley.quangson@gmail.com>
2023-10-07 19:05:32 -0600

    created controllers for client side to call. Second Get request in Forecast isnt working, so made TempController

---
+++ NWSAnalysis/Server/Controllers/ForecastController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NWSAnalysis.Server.Services;
+using NWSAnalysis.Shared.Models;
+
+namespace NWSAnalysis.Server.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ForecastController : ControllerBase
+{
+    private readonly IApiService _apiService;
+
+    public ForecastController(IApiService apiService)
+    {
+        _apiService = apiService;
+    }
+
+    [HttpGet("{state}")]
+    public async Task<ForecastDetails> GetStationsApi(string state)
+    {
+        return await _apiService.GetStationNames(state);
+
+    }
+
+    [HttpGet("readings/{stationId}")]
+    public async Task<Dictionary<DateTime, float?>> GetForecast(string stationId)
+    {
+        return await _apiService.GetStationForecasts(stationId);
+    }
+}
---
+++ NWSAnalysis/Server/Controllers/TempController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NWSAnalysis.Server.Services;
+
+namespace NWSAnalysis.Server.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class TempController : ControllerBase
+{
+    private readonly IApiService _apiService;
+
+    public TempController(IApiService apiService)
+    {
+        _apiService = apiService;
+    }
+
+    [HttpGet("{stationId}")]
+    public async Task<Dictionary<DateTime, float?>> GetForecast(string stationId)
+    {
+        return await _apiService.GetStationForecasts(stationId);
+    }
+}
