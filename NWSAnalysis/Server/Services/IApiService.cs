c2ba5e25cab4740b3e6ae755c1737a26b3391984
Bradley Quangson <bradley.quangson@gmail.com>
2023-10-07 19:04:36 -0600

    updated services needed for client side

---
+++ NWSAnalysis/Server/Services/IApiService.cs
@@ -0,0 +1,10 @@
+﻿using NWSAnalysis.Shared.Models;
+
+namespace NWSAnalysis.Server.Services;
+
+public interface IApiService
+{
+    Task<ForecastDetails?> GetStationNames(string stateCode);
+    Task<Dictionary<DateTime, float?>> GetStationForecasts(string stationId);
+
+}
